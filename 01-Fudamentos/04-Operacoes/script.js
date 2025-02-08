cba6449d6bd174ff12800ed60443360d4fcc651b
pedroggwp <phsnnascimento@outlook.com>
2025-02-08 20:47:36 -0300

    Operações com variáveis

---
+++ 01-Fudamentos/04-Operacoes/script.js
@@ -0,0 +1,27 @@
+// Operações com variáveis tipo number
+let x = 2;
+x = x + 1;
+
+console.log(x); // 3
+
+let y = 2;
+y = y / 8;
+console.log(y); // 0.25
+
+let z = 2;
+z = z * 7;
+console.log(z); // 14
+
+
+// Operações com variáveis tipo string
+let nomeStr = "Pedro";
+nomeStr = nomeStr + 8;
+
+console.log(nomeStr); // Pedro8
+
+
+let nome = "Pedro";
+let sobrenome = "Nascimento";
+
+let nomeCompleto = nome + " " + sobrenome;
+console.log(nomeCompleto); // Pedro Nascimento
