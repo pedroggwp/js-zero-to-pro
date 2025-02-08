13fcff9ad44dd6e488e5090a968b8a26613d5ccf
pedroggwp <phsnnascimento@outlook.com>
2025-02-08 20:25:25 -0300

    Exercício 1 - Variáveis

---
+++ Exercicios/01-Variaveis/script.js
@@ -0,0 +1,20 @@
+// 1. Crie uma variável para armazenar o nome do carro e coloque o valor Ferrari. E exiba-a.
+const car = "Ferrari"
+console.log(car)
+
+// 2. Crie uma variável para armazenar o preço do bolo, que custa R$19.99. E exiba-a.
+const cakePrice = 19.99
+console.log(`Preço do bolo: R$${cakePrice}`)
+
+// 3. Corrija o código a seguir (e exiba-o)
+// let "cidade" = São Paulo
+let cidade = "São Paulo"
+console.log(cidade)
+
+/*
+
+Ferrari
+Preço do bolo: R$19.99
+São Paulo
+
+*/
