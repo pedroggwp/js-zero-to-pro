1f70f51cf21d28cc5ee4949af4fe5e0471ecae30
pedroggwp <phsnnascimento@outlook.com>
2025-02-08 20:15:23 -0300

    Tipos de dados no Javascript

---
+++ 01-Fudamentos/03-Tipo-De-Dados/03-tipos-de-dados.js
@@ -0,0 +1,14 @@
+let nome = "Pedro"
+let idade = 18
+let logado = true
+let ingredientes = ["Ovos", "Leite", "Farinha de Trigo"]
+let pedro = {
+    nome: "Pedro",
+    idade: 18
+}
+
+console.log(typeof(nome)) // string
+console.log(typeof(idade)) // number
+console.log(typeof(logado)) // boolean
+console.log(typeof(ingredientes)) // object
+console.log(typeof(pedro)) // object
