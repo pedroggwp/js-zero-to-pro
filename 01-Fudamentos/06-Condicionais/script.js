816864cc691b004be1887d232a526d966156a2fe
pedroggwp <phsnnascimento@outlook.com>
2025-02-08 21:56:57 -0300

    condicionais & multicondicionais

---
+++ 01-Fudamentos/06-Condicionais/script.js
@@ -0,0 +1,43 @@
+// let idade = 90;
+
+// if (idade >= 18) {
+//     console.log('Maior de idade');
+// } else {
+//     console.log('Menor de idade');
+// }
+
+
+
+// Diferença entre == e ===
+// let idade = 20;
+
+// if (idade == 20) {
+//     console.log('Você tem 20 anos');
+// }
+
+// idade = "20";
+
+// if (idade == 20) {
+//     console.log('Você tem 20 anos');
+// }
+
+// idade = 20;
+// if (idade === 20) {
+//     console.log('Você tem 20 anos');
+// }
+
+// idade = "20";
+// if (idade === 20) {
+//     console.log('Você tem 20 anos');
+// }
+
+
+let idade = 90;
+
+if (idade < 18) {
+    console.log('CRIANÇA')
+} else if (idade >= 18 && idade < 60) {
+    console.log('ADULTO')
+} else if (idade >= 60) {
+    console.log('IDOSO')
+}
