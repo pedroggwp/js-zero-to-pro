c372d8ed56182e634f800897c241e4c1d2349054
pedroggwp <phsnnascimento@outlook.com>
2025-02-08 22:10:34 -0300

    Switch Case

--- 01-Fudamentos/06-Condicionais/script.js
+++ 01-Fudamentos/06-Condicionais/script.js
@@ -32,12 +32,39 @@
 // }
 
 
-let idade = 90;
+// let idade = 90;
 
-if (idade < 18) {
-    console.log('CRIANÇA')
-} else if (idade >= 18 && idade < 60) {
-    console.log('ADULTO')
-} else if (idade >= 60) {
-    console.log('IDOSO')
+// if (idade < 18) {
+//     console.log('CRIANÇA')
+// } else if (idade >= 18 && idade < 60) {
+//     console.log('ADULTO')
+// } else if (idade >= 60) {
+//     console.log('IDOSO')
+// }
+
+
+// Operador ternário
+// let isMember = true;
+// let shipping = isMember ? 2 : 10;
+
+// console.log(isMember ? 'É MEMBRO' : 'NÃO É MEMBRO')
+// console.log(`Frete: R$${shipping}`)
+
+// Switch
+let profession = 'pedreiro';
+console.log(`Profissão : ${profession}`)
+
+switch (profession) {
+    case 'Fiscal':
+        console.log('CAMISA VERDE')
+        break;
+    case 'Bombeiro':
+        console.log('CAMISA VERMELHA')
+        break;
+    case 'Policial':
+        console.log('CAMISA AZUL')
+        break;
+    default:
+        console.log('CAMISA PRETA')
+        break;
 }
